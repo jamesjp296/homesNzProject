a3035cdb0c2e5cf23d4788be95d8a62a660ca439
James Joy <jamesjp296@gmail.com>
2020-08-30 11:07:45 +1200

    Reading data from text file

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/jamesjp296/homesNzProject/model"
 	"log"
 	"time"
 )
@@ -8,5 +10,11 @@ import (
 func main(){
 	now := time.Now()
 
+	propertiesSlice, err :=model.GetPropertyDetails()
+	if err != nil {
+		log.Fatal("Error while getting property details")
+	}
+
+	fmt.Println("The length of the properites Slice ", len(propertiesSlice))
 	log.Printf("Time taken to process results : %v", time.Now().Sub(now).String())
 }
--- model/propertyDetails.go
+++ model/propertyDetails.go
@@ -1,7 +1,77 @@
 package model
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func Greet(){
-	fmt.Println("Hello from Property")
+type Property struct {
+	ID            string
+	StreetAddress string
+	Town          string
+	ValuationDate string
+	Value         int
+}
+
+
+func GetPropertyDetails()([]Property, error){
+	var textFileRows []string
+
+
+	file, err := os.Open("properties.txt")
+	//file, err := os.Open("testproperties.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		textFileRows = append(textFileRows, scanner.Text())
+	}
+	file.Close()
+
+	return getPropertyStruct(textFileRows),nil
+
+}
+
+func getPropertyStruct(textFileRows []string) []Property {
+	var propertyDetailsSlice []Property
+	for i, row := range textFileRows {
+
+		if len(strings.TrimSpace(row)) != 0 && i != 0 {
+
+			column := strings.Split(row, "	")
+			var prop Property
+
+			if len(strings.TrimSpace(column[0])) != 0 {
+				prop.ID = column[0]
+			}
+
+			if len(strings.TrimSpace(column[1])) != 0 {
+				prop.StreetAddress = column[1]
+			}
+			if len(strings.TrimSpace(column[2])) != 0 {
+				prop.Town = column[2]
+			}
+			if len(strings.TrimSpace(column[3])) != 0 {
+				prop.ValuationDate = column[3]
+			}
+			if len(strings.TrimSpace(column[4])) != 0 {
+				var propertyValue, err = strconv.Atoi(column[4])
+				if err != nil {
+					fmt.Println(err)
+				}
+				prop.Value = propertyValue
+			}
+
+			propertyDetailsSlice = append(propertyDetailsSlice, prop)
+
+		}
+	}
+
+	return propertyDetailsSlice
 }
