e65c8c58022a325bf8fe9d119536a76ec01dc8da
James Joy <jamesjp296@gmail.com>
2020-08-30 11:27:27 +1200

    Getting the firstEntRecord

--- main.go
+++ main.go
@@ -17,10 +17,16 @@ func main() {
 	fmt.Println("The length of the properites Slice ", len(propertiesSlice))
 
 	/*-------------------Test #1 : In the case of duplicates, use the last encountered record ----------------------*/
-	fmt.Println("Test #1 : In the case of duplicates, use the last encountered record")
+	fmt.Println("-------Test #1 : In the case of duplicates, use the last encountered record-------------")
 	laslEntRecordsMap := GetLastEntRecords(propertiesSlice)
 	fmt.Println("The length of last encountered records: ", len(laslEntRecordsMap))
 	display(laslEntRecordsMap)
+
+	/*-------------------Test #2 : In the case of duplicates, use the first encountered record ----------------------*/
+	fmt.Println("-------------Test #2 : In the case of duplicates, use the first encountered record-------------")
+	firstEntRecordsMap := GetFirstEntRecords(propertiesSlice)
+	fmt.Println("The length of first encountered records: ", len(firstEntRecordsMap))
+	display(firstEntRecordsMap)
 
 	log.Printf("Time taken to process results : %v", time.Now().Sub(now).String())
 }
--- propertyDetailsUtil.go
+++ propertyDetailsUtil.go
@@ -21,3 +21,20 @@ func GetLastEntRecords(propertiesSlice []PropertyValue) map[PropertyDetails]int 
 	}
 	return propertyMap
 }
+
+func GetFirstEntRecords(propertiesSlice []PropertyValue) map[PropertyDetails]int {
+	var propertyMap = make(map[PropertyDetails]int)
+	for _, prop := range propertiesSlice {
+		propMapKey := PropertyDetails{
+			StreetAddress: prop.StreetAddress,
+			Town:          prop.Town,
+			ValDate:       prop.ValuationDate,
+		}
+
+		if _, ok := propertyMap[propMapKey]; !ok {
+			propertyMap[propMapKey] = prop.Value
+		}
+
+	}
+	return propertyMap
+}
